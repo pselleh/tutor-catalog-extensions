c0e96b9f6e13a33d5351fea3a5287560225f9e5c
Phil Selleh <pselleh@gmail.com>
2026-05-06 13:48:28 +0000

    Fix: install correct Django app repo

--- tutor_catalog_extensions/plugin.py
+++ tutor_catalog_extensions/plugin.py
@@ -1,6 +1,5 @@
 from tutor import hooks
 
-# Install Django app (correct repo)
 hooks.Filters.CONFIG_DEFAULTS.add_items([
     (
         "DISCOVERY_EXTRA_PIP_PACKAGES",
@@ -8,7 +7,6 @@ hooks.Filters.CONFIG_DEFAULTS.add_items([
     ),
 ])
 
-# Register app in Discovery
 hooks.Filters.ENV_PATCHES.add_item((
     "discovery-production-settings",
     """
