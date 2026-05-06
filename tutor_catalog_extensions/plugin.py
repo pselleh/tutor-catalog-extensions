344c8b5e2bc9e99642c91fcff4f965be1f9275a8
Phil Selleh <pselleh@gmail.com>
2026-05-06 13:41:43 +0000

    Final production plugin

--- tutor_catalog_extensions/plugin.py
+++ tutor_catalog_extensions/plugin.py
@@ -1,10 +1,17 @@
-print("🔥 catalog_extensions plugin LOADED")
-
 from tutor import hooks
 
+# Install your Django app from GitHub
+hooks.Filters.CONFIG_DEFAULTS.add_items([
+    (
+        "DISCOVERY_EXTRA_PIP_PACKAGES",
+        "git+https://github.com/pselleh/catalog-extensions.git@main",
+    ),
+])
+
+# Register Django app in Discovery
 hooks.Filters.ENV_PATCHES.add_item((
     "discovery-production-settings",
     """
-INSTALLED_APPS.append("catalog_extensions")
+INSTALLED_APPS.append("cba_catalog_extensions.apps.CbaCatalogExtensionsConfig")
 """
 ))
