90d825acef1874250e22c36e4dcd971aad83eabd
Phil Selleh <pselleh@gmail.com>
2026-05-06 01:12:10 +0000

    add apps.py for Django app

--- tutor_catalog_extensions/cba_catalog_extensions/apps.py
+++ tutor_catalog_extensions/cba_catalog_extensions/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-class CatalogExtensionsConfig(AppConfig):
+
+class CbaCatalogExtensionsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "catalog_extensions"
+    name = "cba_catalog_extensions"
