e9baa84208c2cf7ac533d5b99187bb8b0e32de05
Phil Selleh <pselleh@gmail.com>
2026-05-12 17:35:57 +0000

    Use proper Tutor 21 build hooks

--- tutor_catalog_extensions/plugin.py
+++ tutor_catalog_extensions/plugin.py
@@ -4,47 +4,51 @@ from tutor import hooks
 
 
 # ---------------------------------------------------
-# 1. Copy package into Open edX image build context
+# 1. Add local package to openedx image build
 # ---------------------------------------------------
-hooks.Filters.IMAGES_BUILD.add_item((
-    "openedx",
-    ("catalog-extensions", "/openedx/catalog-extensions"),
-))
+hooks.Filters.IMAGES_BUILD.add_item(
+    (
+        "openedx",
+        str("/home/cbaadmin/src/catalog-extensions"),
+        "/openedx/catalog-extensions",
+    )
+)
 
 
 # ---------------------------------------------------
-# 2. Install package into Open edX image
+# 2. Install package requirements
 # ---------------------------------------------------
-hooks.Filters.ENV_PATCHES.add_item((
-    "openedx-dockerfile-post-python-packages",
-    """
-RUN pip install -e /openedx/catalog-extensions
-"""
-))
+hooks.Filters.OPENEDX_EXTRA_PIP_REQUIREMENTS.add_item(
+    "file:///openedx/catalog-extensions"
+)
 
 
 # ---------------------------------------------------
 # 3. Register Django app in Discovery
 # ---------------------------------------------------
-hooks.Filters.ENV_PATCHES.add_item((
-    "discovery-common-settings",
-    """
+hooks.Filters.ENV_PATCHES.add_item(
+    (
+        "discovery-common-settings",
+        """
 if "catalog_extensions" not in INSTALLED_APPS:
     INSTALLED_APPS.append("catalog_extensions")
-"""
-))
+""",
+    )
+)
 
 
 # ---------------------------------------------------
 # 4. Register Discovery URLs
 # ---------------------------------------------------
-hooks.Filters.ENV_PATCHES.add_item((
-    "discovery-root-urlconf",
-    """
+hooks.Filters.ENV_PATCHES.add_item(
+    (
+        "discovery-root-urlconf",
+        """
 from django.conf.urls import include, url
 
 urlpatterns += [
     url(r"^api/catalog/", include("catalog_extensions.urls")),
 ]
-"""
-))
+""",
+    )
+)
