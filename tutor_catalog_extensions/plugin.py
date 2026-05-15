682ac13a69e957eea0c609409a5abe8baa61644f
Phil Selleh <pselleh@gmail.com>
2026-05-15 15:00:43 +0000

    Fix Open edX MFE auth, CSRF, CORS, and shared cookie configuration

--- tutor_catalog_extensions/plugin.py
+++ tutor_catalog_extensions/plugin.py
@@ -65,12 +65,12 @@ hooks.Filters.ENV_PATCHES.add_item(
     (
         "openedx-lms-common-settings",
         """
-CSRF_TRUSTED_ORIGINS = [
+CSRF_TRUSTED_ORIGINS = list(dict.fromkeys([
     "https://eadvantage.com",
     "https://apps.eadvantage.com",
     "https://studio.eadvantage.com",
     "https://discovery.eadvantage.com",
-]
+]))
 
 SESSION_COOKIE_DOMAIN = ".eadvantage.com"
 
@@ -90,11 +90,11 @@ hooks.Filters.ENV_PATCHES.add_item(
     (
         "openedx-lms-common-settings",
         """
-CORS_ORIGIN_WHITELIST = [
+CORS_ORIGIN_WHITELIST = list(dict.fromkeys([
     "https://eadvantage.com",
     "https://apps.eadvantage.com",
     "https://studio.eadvantage.com",
-]
+]))
 """,
     )
 )
