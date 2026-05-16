dc94ba19d4c9334198516eb37261895baaf57b19
Phil Selleh <pselleh@gmail.com>
2026-05-16 00:24:50 +0000

    Fix production Tutor plugin packaging and metadata

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[],
     entry_points={
         "tutor.plugin.v1": [
             "tutor_catalog_extensions = tutor_catalog_extensions.plugin",
