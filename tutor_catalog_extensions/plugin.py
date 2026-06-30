afc81d5ec5276b77d1d3a18a960f590a895dc452
Phil Selleh <pselleh@gmail.com>
2026-06-30 15:39:37 +0000

    Simplify Discovery plugin

--- tutor_catalog_extensions/plugin.py
+++ tutor_catalog_extensions/plugin.py
@@ -1,121 +1,12 @@
-print("🔥 tutor_catalog_extensions plugin LOADED")
-
 from tutor import hooks
 
-
-# ---------------------------------------------------
-# Add catalog_extensions package to Discovery build
-# ---------------------------------------------------
-hooks.Filters.ENV_PATCHES.add_item(
-    (
-        "private.txt",
-        """
--e /openedx/catalog-extensions
-""",
-    )
-)
-
-
-# ---------------------------------------------------
-# Copy package into build context
-# ---------------------------------------------------
-hooks.Filters.MOUNTED_DIRECTORIES.add_item(
-    (
-        "openedx",
-        "/home/cbaadmin/src/catalog-extensions",
-        "/openedx/catalog-extensions",
-    )
-)
-
-
-# ---------------------------------------------------
-# Register Django app in Discovery
-# ---------------------------------------------------
+# Register the Django app in Discovery
 hooks.Filters.ENV_PATCHES.add_item(
     (
         "discovery-common-settings",
         """
 if "catalog_extensions" not in INSTALLED_APPS:
     INSTALLED_APPS.append("catalog_extensions")
-""",
-    )
-)
-
-
-# ---------------------------------------------------
-# Register Discovery URLs
-# ---------------------------------------------------
-hooks.Filters.ENV_PATCHES.add_item(
-    (
-        "discovery-root-urlconf",
-        """
-from django.conf.urls import include, url
-
-urlpatterns += [
-    url(r"^api/catalog/", include("catalog_extensions.urls")),
-]
-""",
-    )
-)
-
-# ---------------------------------------------------
-# LMS CSRF / cookie fixes
-# ---------------------------------------------------
-hooks.Filters.ENV_PATCHES.add_item(
-    (
-        "openedx-lms-common-settings",
-        """
-CSRF_TRUSTED_ORIGINS = list(dict.fromkeys([
-    "https://centerforbusinessacceleration.com",
-    "https://apps.centerforbusinessacceleration.com",
-    "https://studio.centerforbusinessacceleration.com",
-    "https://discovery.centerforbusinessacceleration.com",
-]))
-
-SESSION_COOKIE_DOMAIN = ".centerforbusinessacceleration.com"
-
-CSRF_COOKIE_DOMAIN = ".centerforbusinessacceleration.com"
-
-CSRF_COOKIE_SECURE = True
-
-SESSION_COOKIE_SECURE = True
-""",
-    )
-)
-
-# ---------------------------------------------------
-# LMS CORS fixes
-# ---------------------------------------------------
-hooks.Filters.ENV_PATCHES.add_item(
-    (
-        "openedx-lms-common-settings",
-        """
-CORS_ORIGIN_WHITELIST = list(dict.fromkeys([
-    "https://centerforbusinessacceleration.com",
-    "https://apps.centerforbusinessacceleration.com",
-    "https://studio.centerforbusinessacceleration.com",
-]))
-""",
-    )
-)
-
-# ---------------------------------------------------
-# Production LMS Redis session/cache settings
-# ---------------------------------------------------
-hooks.Filters.ENV_PATCHES.add_item(
-    (
-        "openedx-lms-common-settings",
-        """
-CACHES["default"] = {
-    "BACKEND": "django_redis.cache.RedisCache",
-    "LOCATION": "redis://redis:6379/1",
-    "OPTIONS": {
-        "CLIENT_CLASS": "django_redis.client.DefaultClient",
-    },
-}
-
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-SESSION_CACHE_ALIAS = "default"
 """,
     )
 )
